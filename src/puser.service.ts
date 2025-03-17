88db0a705a582422e35f6b292ff2d0312c368515
Phạm Nguyên Khoa <pnkvlog1508@gmail.com>
2025-03-17 16:57:05 +0700

    dsd

--- src/puser.service.ts
+++ src/puser.service.ts
@@ -14,6 +14,7 @@ export class PuserService {
             useTLS: true
         })
     }
+
     async message(channel: string, event: string, data: any) {
         await this.pusher.trigger(channel, event, data)
     }
